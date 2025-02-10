09f69e7f81c8dedd2db5b6600736ea0657239475
bradyDarren <dcgbfinancial@gmail.com>
2025-02-10 14:04:48 -0600

    addition learning of pandas

--- main.py
+++ main.py
@@ -18,18 +18,50 @@
 
 import pandas
 
+# importing tabular data into a script
 data_p = pandas.read_csv("weather_data.csv")
 # print(data_p)
-# print(data_p['temp'])
 
-data_dict = data_p.to_dict()
-# print(data_dict)
+# # isolating and printing a specific columns within our Data Frame.
+# # print(data_p['temp'])
 
-data_list = data_p['temp'].to_list()
-print(data_list)
+# # conversiting our data from from a tabular form to a dictionary.
+# data_dict = data_p.to_dict()
+# # print(data_dict)
 
-total_temp = 0 
-for temp in data_list: 
-    total_temp += temp
-average_temp = total_temp/len(data_list)
-print(average_temp)
+# # i could also use the sum() method for iterables, or the mean() method in pandas
+# # printing the average of all the temps within the temp column.
+# #  total_temp = 0 
+# # for temp in temp_list: 
+# #     total_temp += temp
+# # average_temp = total_temp/len(temp_list)
+# # print(average_temp)
+
+# # using the built in methods of panda to obtain the highest value within the temp column.
+max_temp = data_p['temp'].max()
+# print(max_temp)
+
+# getting data that is within the rows of our entire data frame.
+first_day = data_p[data_p.day == 'Monday']
+# print(first_day)
+ 
+# getting the row of data that had the highest temperature of the week. 
+day_of_max_temp = data_p[data_p.temp == data_p.temp.max()]
+# print(day_of_max_temp)
+
+# isolating values within a rows that meets a specific condition
+monday = data_p[data_p.day == "Monday"]
+# print(monday.temp)
+# print(monday.temp[0]) # removes the row number from the output.
+# print((monday.temp[0])*(9/5) + 32)
+
+#create a data from from scratch
+
+data_dict  = {
+    'students': ['Amy', 'James', 'Angela'],
+    'scores': [76,56,65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv('new_data.csv')
+# print(data)
