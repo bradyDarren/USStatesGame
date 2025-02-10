ef64f100f6cfa1d0527bac52d0a69c0e53cdfbe3
bradyDarren <dcgbfinancial@gmail.com>
2025-02-10 11:37:07 -0600

    experimenting with pandas

--- main.py
+++ main.py
@@ -4,13 +4,32 @@
 #     data = file.readlines()
 #     print(data)
 
-import csv 
+# import csv 
 
-with open(file='weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    count = 0 
-    for row in data:
-        if row[1] != 'temp':
-           temperature.append(int(row[1]))
-    print(temperature)
+# with open(file='weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     count = 0 
+#     for row in data:
+#         if row[1] != 'temp':
+#            temperature.append(int(row[1]))
+#     print(temperature)
+
+
+import pandas
+
+data_p = pandas.read_csv("weather_data.csv")
+# print(data_p)
+# print(data_p['temp'])
+
+data_dict = data_p.to_dict()
+# print(data_dict)
+
+data_list = data_p['temp'].to_list()
+print(data_list)
+
+total_temp = 0 
+for temp in data_list: 
+    total_temp += temp
+average_temp = total_temp/len(data_list)
+print(average_temp)
